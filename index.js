1d5a0cb2d0dc97004a7b2c3e6aa98245c8e696d9
Jan Baerts <janbaerts@gmail.com>
2024-01-17 16:03:20 +0100

    Checking request data.

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('node:fs');
 
 const app = express();
 
+let isSavingToFile = false;
+
 app.use(bodyParser.json());
 
-app.post('/garmin/webhook', (request, result) => {
-    console.log('Event received: ', request.body);
+app.post('/garmin/webhook', async (request, result) => {
+    while (isSavingToFile)
+        await sleep(100);
+
+    isSavingToFile = true;
+    console.log('Event received...', request.body);
+    console.log('Request data:');
+    // await appendDataToUserDataFile(request.body.data.message);
+    console.log(request);
+    console.log('Result data:');
+    console.log(result);
     result.status(200).send('OK');
+    isSavingToFile = false;
 });
 
 const PORT = 3008;
@@ -15,3 +28,23 @@ const PORT = 3008;
 app.listen(PORT, () => {
     console.log(`Now listening on port ${PORT}...`);
 });
+
+async function appendDataToUserDataFile(data)
+{
+    if (data == undefined)
+        return;
+
+    await fs.writeFile('userdata.json', data + '\n', { flag: 'a+' }, callback => {
+        if (callback == null)
+        {
+            console.log("Data saved to file.");
+            isSavingToFile = false;
+            return;
+        }
+    });
+}
+
+function sleep(ms)
+{
+    return new Promise((resolve) => setTimeout(resolve, ms));
+}
